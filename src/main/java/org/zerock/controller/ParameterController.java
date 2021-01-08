f49e3696c29fcc67b7ebe607459713bc7fc6a8e4
pedroyoung95 <jty9531@gmail.com>
2021-01-08 15:48:35 +0900

    1/8 15:48 수업내용

---
+++ src/main/java/org/zerock/controller/ParameterController.java
@@ -0,0 +1,98 @@
+package org.zerock.controller;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.bean.Member;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/paramex/*")
+@Log4j
+public class ParameterController {
+
+	@RequestMapping("/ex1")
+	public void method1(HttpServletRequest request) {
+		log.info(request);
+		log.info(request.getParameter("name"));
+		log.info("method1");
+		//request객체를 파라미터로 받아서 그대로 사용하는 방식으로 파라미터 수집
+	}
+	
+	@RequestMapping("/ex2")
+	public void method2(@RequestParam("name") String n) {
+		//메소드의 파라미터에 @RequestParam어노테이션을 붙여서 파라미터 수집 가능
+		log.info("method2");
+		log.info(n);
+	}
+	
+	@RequestMapping("/ex3")
+	public void method3(@RequestParam String name) {
+		//받을 파라미터와 메소드의 변수 이름이 같으면 파라미터 이름 생략 가능
+		log.info("method3");
+		log.info(name);
+	}
+	
+	@RequestMapping("/ex4")
+	public void method4(String name) {
+		//받을 파라미터와 메소드의 변수 이름이 같으면 어노테이션도 생략 가능
+		log.info("method4");
+		log.info(name);
+	}
+	
+	@RequestMapping("/ex5")
+	public void method5(HttpServletRequest request) {
+		log.info("method5");
+		log.info(request.getParameterValues("check"));
+		String[] list = request.getParameterValues("check");
+		for(String s : list) {
+			log.info(s);
+		}
+	}
+	
+	@RequestMapping("/ex6")
+	public void method6(String[] check) {
+		log.info("method6");
+		for(String s : check) {
+			log.info(s);
+		}
+	}
+	
+	@RequestMapping("/ex7")
+	public void method7(@RequestParam("check") ArrayList<String> check) {
+		//리스트는 파라미터와 이름이 같아도 @RequestParam 생략 불가
+		log.info("method7");
+		log.info(check);
+		for(String c : check) {
+			log.info(c);
+		}
+	}
+	
+	@RequestMapping("/ex8")
+	public void method8(HttpServletRequest request) {
+		String name = request.getParameter("name");
+		String ageStr = request.getParameter("age");
+		int age = Integer.parseInt(ageStr);
+		
+		Member member = new Member();
+		member.setName(name);
+		member.setAge(age);
+		
+		log.info("method8");
+		log.info(member);
+	}
+	
+	@RequestMapping("/ex9")
+	public void method9(Member member) {
+		//파라미터이름과 필드이름이 같은 걸 매칭해서 객체에 적절히 값을 자동으로 넣어줌
+		//파라미터값을 받아서 객체에 set하는 코드가 필요X
+		log.info("method9");
+		log.info(member);
+	}
+	
+}
--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,9 +1,13 @@
 package org.zerock.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.log4j.Log4j;
 
@@ -21,5 +25,27 @@ public class SampleController {
 	@GetMapping("/basicOnlyGet")
 	public void basicGet2() {
 		log.info("basic get only get.................");
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+		log.info("name: " + name);
+		log.info("age: " + age);
+		return "ex02";
+	}
+	
+	@GetMapping("/ex02Array")
+	public String ex02Array(@RequestParam("ids") String[] ids) {
+		log.info("array ids: " + Arrays.toString(ids));
+		for(String s : ids) {
+			log.info(s);
+		}
+		return "ex02Array";
+	}
+	
+	@GetMapping("/ex02List")
+	public String ex02List(@RequestParam("ids") ArrayList<String> ids) {
+		log.info("ids: " + ids);
+		return "ex02List";
 	}
 }
